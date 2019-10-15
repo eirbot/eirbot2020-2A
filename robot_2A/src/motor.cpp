c1a4ea05d0ddd87a058f6020667f2eb9cc3fd9c3
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-10-15 08:58:33 +0200

    robot_2A: added motor class

---
+++ robot_2A/src/motor.cpp
@@ -0,0 +1,11 @@
+#include "motor.hpp"
+
+
+Motor::Motor(void) {
+  
+}
+
+Motor::setRatio(int16_t ratio) {
+
+}
+
---
+++ robot_2A/src/motor.hpp
@@ -0,0 +1,20 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <cstdint>
+
+/* Class to work with a motor alone. */
+class Motor {
+   
+public:
+  Motor(void);
+
+  /* Set the Ration of the PWM. The value can go from 0
+   * to 2^16 */ 
+  void setRatio(int16_t ratio);
+
+private:
+  
+};
+
+#endif MOTOR_H
