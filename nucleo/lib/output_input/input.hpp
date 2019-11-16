de29a7b7aa67ceb5706aa59ba1a581e8beb83da4
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-11-16 16:46:44 +0100

    nucleo: added input/output class

---
+++ nucleo/lib/output_input/input.hpp
@@ -0,0 +1,10 @@
+#ifndef __INPUT_H_
+#define __INPUT_H_
+
+template<typename T>
+class Input {
+public:
+  T get(T value);
+};
+
+#endif // __INPUT_H_
---
+++ nucleo/lib/output_input/output.hpp
@@ -0,0 +1,10 @@
+#ifndef __OUTPUT_H_
+#define __OUTPUT_H_
+
+template<typename T>
+class Output {
+public:
+  void put(T value);
+};
+
+#endif // __OUTPUT_H_
