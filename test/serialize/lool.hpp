998d25772d27866da5721e99824d0df609e73557
Loïc Dauphin <astralien3000@yahoo.fr>
2019-12-06 23:33:58 +0100

    manage several int types with SimpleDynamicRandomAccessStream
    Signed-off-by: Loïc Dauphin <astralien3000@yahoo.fr>

--- test/serialize/lool.hpp
+++ test/serialize/lool.hpp
@@ -3,27 +3,29 @@
 
 #include "archive.hpp"
 
-struct Lool {
-  int a;
-  int b;
-  int c;
-  int d;
+#include <cstdint>
 
-  int getD(void) { return d; }
-  void setD(int val) { d = val; }
+struct Lool {
+  uint64_t a;
+  uint8_t b;
+  uint32_t c;
+  int16_t d;
+
+  int16_t getD(void) { return d; }
+  void setD(int16_t val) { d = val; }
 };
 
 namespace archive {
 
 template<typename Archive>
 Archive& operator<<(Archive& ar, Lool& lool) {
-  ar << lool.b;
+  ar << lool.a;
 
-  ar << make_named("a", lool.a);
+  ar << make_named("b", lool.b);
 
   ar << lool.c;
 
-  int d = lool.getD();
+  decltype(lool.d) d = lool.getD();
   ar << make_named("d", d);
   lool.setD(d);
 
