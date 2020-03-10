c9bc372f95406439126331268384218b0707983a
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2020-03-10 18:22:25 +0100

    Com: Protocol is in a class

--- ProtocolCom/rpi/com.py
+++ ProtocolCom/rpi/com.py
@@ -8,11 +8,40 @@ import serial
 import struct
 import logging
 
-SERIAL_PORT = "/dev/ttyACM0"
+SERIAL_PORT = "/dev/ttyACM1"
 
 logging.basicConfig(level=logging.DEBUG)
 
 
+class Com:
+    def __init__(self, serial_port):
+        self.serial = serial.Serial(serial_port, baudrate=115200)
+
+    def ask(self, feature, *args):
+        payload = feature.identifier.to_bytes(1, "little")
+        for arg in args:
+            if isinstance(arg, int):
+                payload += struct.pack("i", arg)
+            elif isinstance(arg, float):
+                payload += struct.pack("f", arg)
+        logging.debug("%s: Sending %s", feature.name, payload)
+        self.serial.write(payload)
+
+    def loop(self):
+        logging.debug("Waiting data from serial")
+        while True:
+            request = self.serial.read(1)
+            logging.debug("RECEIVED: %s", request)
+            self.parse(request.decode())
+            ack = self.serial.read(1)
+            logging.debug("RECEIVED ack")
+
+    def parse(self, payload):
+        feature_id = ord(payload[0])
+        if feature_id in range(len(FEATURES)):
+            FEATURES[feature_id].handle(self.serial)
+        else:
+            logging.error("parse: id %d not recognized", feature_id)
 
 
 class Feature:
@@ -28,46 +57,36 @@ class Feature:
         self.handle = handler
 
 
-def ask(feature, *args):
-    payload = feature.identifier.to_bytes(1, "little")
-    for arg in args:
-        if isinstance(arg, int):
-            payload += struct.pack("i", arg)
-        elif isinstance(arg, float):
-            payload += struct.pack("f", arg)
-    logging.debug("%s: Sending %s", feature.name, payload)
-    return payload
+def handle_get_pos(serial):
+    logging.debug("handle_get_pos: reading 12 bytes")
+    pos = serial.read(12)
+    x, y, t = struct.unpack("fff", pos)
+    print(f"x: {x}, y: {y}, t: {t}")
 
 
-def parse(payload):
-    feature_id = payload[0]
-    return FEATURES[feature_id].handle(payload[1:])
+def handle_void(serial):
+    pass
 
 
 def simple_handle_decode(decode_str):
     """
     Return a simple handler that return a tuple depending on `decode_str`
     """
-    return lambda x: struct.unpack(decode_str, x)
+    return lambda x: print(struct.unpack(decode_str, x))
 
 
-get_pos = Feature("get_pos", 0, simple_handle_decode("fff"))
-set_pos = Feature("set_pos", 1, simple_handle_decode("c"))
-goto = Feature("goto", 2, simple_handle_decode("c"))
-stop = Feature("stop", 3, simple_handle_decode("c"))
-panic = Feature("panic", 4, lambda: print("Panic received"))
+get_pos = Feature("get_pos", 0, handle_get_pos)
+set_pos = Feature("set_pos", 1, handle_get_pos)
+goto = Feature("goto", 2, handle_get_pos)
+stop = Feature("stop", 3, handle_get_pos)
+panic = Feature("panic", 4, handle_get_pos)
 
 FEATURES = [get_pos, set_pos, goto, stop, panic]
 
 
-def main():
-    s = serial.Serial(SERIAL_PORT,baudrate=115200)
-    sending = 0
-    if sending:
-        s.write(ask(get_pos))
-        res = s.read(13)
-        print(parse(res))
-    else:
-        s.write(ask(set_pos, 0.1, 0.3, 0.4))
+import threading
 
-main()
+c = Com(SERIAL_PORT)
+com_thread = threading.Thread(target=c.loop)
+com_thread.start()
+c.ask(get_pos)
